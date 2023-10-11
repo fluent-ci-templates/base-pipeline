850db1cb7121dfdc37d20c8509498d31387014ba
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-10-11 19:32:54 +0000

    update example

---
+++ example/.fluentci/src/dagger/jobs.ts
@@ -0,0 +1,39 @@
+import { client } from "./dagger.ts";
+
+export enum Job {
+  hello = "hello",
+}
+
+export const exclude = [];
+
+export const hello = async (src = ".") => {
+  const context = client.host().directory(src);
+  const ctr = client
+    .pipeline("hello")
+    .container()
+    .from("alpine")
+    .withDirectory("/app", context)
+    .withWorkdir("/app")
+    .withExec(["echo", "'Hello, world!'"]);
+
+  const result = await ctr.stdout();
+
+  return result.replace(/(\r\n|\n|\r)/gm, "");
+};
+
+export type JobExec = (src?: string) =>
+  | Promise<string>
+  | ((
+      src?: string,
+      options?: {
+        ignore: string[];
+      }
+    ) => Promise<string>);
+
+export const runnableJobs: Record<Job, JobExec> = {
+  [Job.hello]: hello,
+};
+
+export const jobDescriptions: Record<Job, string> = {
+  [Job.hello]: "Say hello",
+};
