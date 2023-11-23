a64e58085a0c4ba0dc4ec462929d30edca26fd81
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-11-23 08:16:15 +0300

    export job descriptions

--- src/dagger/index.ts
+++ src/dagger/index.ts
@@ -1,4 +1,4 @@
 import pipeline from "./pipeline.ts";
-import { hello } from "./jobs.ts";
+import { hello, jobDescriptions } from "./jobs.ts";
 
-export { pipeline, hello };
+export { pipeline, hello, jobDescriptions };
